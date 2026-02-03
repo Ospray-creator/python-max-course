1705b8d6fa524521623f359e210be9876f2aec5d
Ospray-creator <lebed89alex@gmail.com>
2026-02-03 14:46:22 +0100

    feat: Продолжение обучения 5

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,10 @@
 import string
 from typing import List, Dict, Tuple
 
+
 def unique_sorted(nums: List[int]) -> List[int] | None:
     """Возвращает отсортированный список уникальных чисел.
-    
+
     Пример
     ------
     >>> unique_sorted([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
@@ -16,15 +17,23 @@ def unique_sorted(nums: List[int]) -> List[int] | None:
 
 def char_counter(text: str) -> Dict[str, int]:
     """Возвращает словарь с количеством вхождений каждого символа в тексте.
-    
+
+    Пример
+    ------
+    >>> char_counter("Hello, World!")
+    {'h': 1, 'e': 1, 'l': 3, 'o': 2, 'w': 1, 'r': 1, 'd': 1}
+
     Использует dict comprehension.
-    
+
     Игнорируй пробелы и знаки пунктуации (string.punctuation).
     """
-    return {char: text.count(char) for char in text if char not in string.punctuation and char != " "}
+    return {
+        char: text.count(char)
+        for char in text
+        if char not in string.punctuation and char != " "
+    }
 
 
 def top_n_common(chars: Dict[str, int], n: int = 5) -> List[Tuple[str, int]]:
-    """Возвращает n самых частых символов, используя sorted(..., key=lambda item: item[1], reverse=True)[:n]."""
+    """Возвращает n самых частых символов"""
     return sorted(chars.items(), key=lambda item: item[1], reverse=True)[:n]
-
